1b35d17b78f7814e6d36584d06e55e4540edb336
Brandon <tobrandonho@gmail.com>
2019-08-26 12:06:11 +0000

    added some stuff

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(page: params[:page], per_page: 2)
@@ -10,7 +10,46 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+  end
+
+  def show
+    @place = Place.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+      @place.update_attributes(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+  end
     redirect_to root_path
   end
 
